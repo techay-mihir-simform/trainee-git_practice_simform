2274072b303483eedc7a0ca9ae89be9ff6010898
techay-mihir-simform <techaypatel123@gmail.com>
2022-02-09 14:40:44 +0530

    Initial commit

---
+++ serverfile.sh
@@ -0,0 +1,30 @@
+#Step 1: ++++++++++++
+#Import this project to your local system : https://github.com/SimformSolutionsPvtLtd/trainee-practical-devops.git
+
+<<comment
+use this command for import project 
+#git clone https://github.com/SimformSolutionsPvtLtd/trainee-practical-devops.git 
+comment
+
+
+#Step 2: ++++++++++++
+#Create new repo and code into repo 
+
+<<comment
+For creating repo in github go to github and create new repo . (trainee-git_practice-simform)
+For push code to github repo use following step :
+
+git init
+git add filename
+git commit -m "Initial commit" filename
+git remote add origin <remoteurl>
+git push -u origin master
+
+comment
+
+
+#Step 3: ++++++++++++
+#Run server.js file 
+
+npm i
+node server.js
